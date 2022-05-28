fc6c96acf6343c54573ff7c218c16f13ace28bd6
unknown <goyalaayush93@gmail.com>
2022-05-28 22:48:18 +0530

    model changes

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import ProductList,Products
 
 # Register your models here.
+
+admin.site.register(ProductList)
+admin.site.register(Products)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import BuyerDetails,SellerDetails
 
 # Register your models here.
+
+admin.site.register(BuyerDetails)
+admin.site.register(SellerDetails)
