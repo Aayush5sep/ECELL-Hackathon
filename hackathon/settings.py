d9346d49d5d48c4665a3d0ad049a69d588ac2379
Aayush <92013588+Aayush5sep@users.noreply.github.com>
2022-06-25 10:30:07 +0530

    Security Key Secured

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-am@biso+*k^qz)d0$f4si#klahq217c41l&2(r8=k^bhsgu(nu'
+SECRET_KEY = '**'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
